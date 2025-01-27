6fc2c3414d5e30c8cb87a8e5f5f50d027673d733
Lakshay Jain <lakshayjain24@gmail.com>
2025-01-28 01:35:48 +0530

    Add user model with schema and JWT token generation method

---
+++ models/user.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],      
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please provide a mobile number"],
+      unique: true,
+    },
+    state: {
+      type: String,
+      enum: [
+        "Andhra Pradesh",
+        "Arunachal Pradesh",
+        "Assam",
+        "Bihar",
+        "Chhattisgarh",
+        "Goa",
+        "Gujarat",
+        "Haryana",
+        "Himachal Pradesh",
+        "Jharkhand",
+        "Karnataka",
+        "Kerala",
+        "Madhya Pradesh",
+        "Maharashtra",
+        "Manipur",
+        "Meghalaya",
+        "Mizoram",
+        "Nagaland",
+        "Odisha",
+        "Punjab",
+        "Rajasthan",
+        "Sikkim",
+        "Tamil Nadu",
+        "Telangana",
+        "Tripura",
+        "Uttar Pradesh",
+        "Uttarakhand",
+        "West Bengal",
+        "Andaman and Nicobar Islands",
+        "Chandigarh",
+        "Dadra and Nagar Haveli and Daman and Diu",
+        "Delhi",
+        "Jammu and Kashmir",
+        "Ladakh",
+        "Lakshadweep",
+        "Puducherry",
+      ],
+      default: "Delhi",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    // enrolledCourses: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Course",
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.generateAuthToken = async function () {
+  const userOptions = { _id: this._id, email: this.email, role: this.role };
+  
+  const token = await jwt.sign(
+    userOptions,
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+  return token;
+};
+
+const user = mongoose.model("User", userSchema);
+
+module.exports = user;
