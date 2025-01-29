0f227656de6c6ea834ee4399900a6dd41e58d13f
Lakshay Jain <lakshayjain24@gmail.com>
2025-01-29 23:49:46 +0530

    Add role router for role management API endpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const authRouter = require('./routes/auth.js');
+const roleRouter = require('./routes/role.js');
 const connectDB = require('./config/connectDB.js');
 
 connectDB();
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use('/api/auth', authRouter);
+app.use('/api/role', roleRouter);
 
 app.get('/',(req,res)=>{
   return res.status(201).send("welcome tester")
