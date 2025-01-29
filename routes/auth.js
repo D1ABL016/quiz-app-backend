e5b7a8a50089544a6d2c3b4b343e623c06840d9d
Lakshay Jain <lakshayjain24@gmail.com>
2025-01-29 23:50:25 +0530

    Add admin role check to authentication routes and update test route path

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authMiddleware = require('../middlewares/auth.js');
+const {authMiddleware , adminMiddleware} = require('../middlewares/auth.js');
 const router = express.Router();
 
 const { register, login } = require('../controllers/auth.js');
@@ -7,7 +7,11 @@ const { register, login } = require('../controllers/auth.js');
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/test-auth',authMiddleware ,(req, res) => {
+router.get('/test/auth',authMiddleware ,(req, res) => {
+  res.status(200).send({'message':'You are authenticated', 'user':req.user});
+});
+
+router.get('/test/role',[authMiddleware , adminMiddleware] ,(req, res) => {
   res.status(200).send({'message':'You are authenticated', 'user':req.user});
 });
 
